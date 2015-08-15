a29b483141236d32d655ef1e94c1d6bd3845cd48
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 01:48:46 +0700

    Update Okhttp

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
@@ -1,29 +1,16 @@
 package com.example.kotchaphanmuangsan.okhttp_json_sqlite_volley;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
-
 
     public final String REDDIT_URL = "https://www.reddit.com/r/all.json?limit=5";
 
@@ -32,38 +19,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyAsyncTask myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute("Sending...", "second");
-    }
+        OkHttpClient client = new OkHttpClient();
 
-    private class MyAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(REDDIT_URL)
-                    .build();
+        Request request = new Request.Builder()
+                .url(REDDIT_URL)
+                .build();
 
-            Response response = null;
-            try {
-                response = client.newCall(request).execute();
-                return response.body().toString();
-            } catch (IOException e) {
-                e.printStackTrace();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                
             }
-            return null;
 
-        }
+            @Override
+            public void onResponse(Response response) throws IOException {
 
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-        }
+            }
+        });
     }
 }
