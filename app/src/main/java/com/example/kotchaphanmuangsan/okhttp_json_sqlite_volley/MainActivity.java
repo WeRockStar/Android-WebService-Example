22de744b34a2b898be02b43c9e97266e550bbd90
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 01:34:36 +0700

    Read data from internet

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
@@ -7,6 +7,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends Activity {
 
 
@@ -18,16 +29,43 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         MyAsyncTask myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute("Sending...", "second", "three");
+        myAsyncTask.execute("Sending...", "second");
     }
 
     private class MyAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
             String param1 = params[0];
-            String param2 = params[1];
-            String param3 = params[2];
-            return "Hello AsyncTask " + param1 + " , " + param2 + " , " + param3;
+
+            String result = "";
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(REDDIT_URL);
+            try {
+                HttpResponse response = httpClient.execute(httpGet, new BasicHttpContext());
+                InputStream is = response.getEntity().getContent();
+                result = inputStreamToString(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        private String inputStreamToString(InputStream is) {
+            String line = "";
+            StringBuilder total = new StringBuilder();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    total.append(line);
+                }
+            } catch (IOException e) {
+
+            }
+
+            return total.toString();
         }
 
         @Override
