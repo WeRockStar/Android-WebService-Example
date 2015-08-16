cc96f8ef385f1bdcaabcbd1fbbcbef7d39d7f217
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 20:07:27 +0700

    Update Singleton pattern

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends Activity {
                 //WHEN AN ERROR
             }
         });
+        //ConnectionManager.getInstance(this).add(request);
         queue.add(request);
     }
 }
