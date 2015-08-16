1e12f12d1bfd3331a35a2828c16991783ddac690
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 23:05:45 +0700

    Write SQL statements

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/database/DBOpenHelper.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/database/DBOpenHelper.java
@@ -8,6 +8,17 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Kotchaphan Muangsan on 16/8/2558.
  */
 public class DBOpenHelper extends SQLiteOpenHelper {
+
+    private static final String TEXT_TYPE = "TEXT";
+    private static final String INTEGER_TYPE = "INTEGER";
+
+    private static final String CREATE_POST_TABLE = "CREATE TABLE "
+            + DatabaseContract.PostTable.TABLE_NAME + " ( "
+            + DatabaseContract.PostTable.TITLE + TEXT_TYPE + " , "
+            + DatabaseContract.PostTable.LINK + TEXT_TYPE + " , "
+            + DatabaseContract.PostTable.IMAGELINK + TEXT_TYPE + " )";
+
+
     public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
