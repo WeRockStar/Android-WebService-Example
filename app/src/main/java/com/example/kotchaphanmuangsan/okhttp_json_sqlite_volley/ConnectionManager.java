2ae79a0fee0e259105928f872867d081a0f838bc
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 20:03:45 +0700

    Add getInstance method

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ConnectionManager.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ConnectionManager.java
@@ -1,7 +1,19 @@
 package com.example.kotchaphanmuangsan.okhttp_json_sqlite_volley;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by Kotchaphan Muangsan on 16/8/2558.
  */
 public class ConnectionManager {
+    private static RequestQueue requestQueue;
+
+    private static RequestQueue getInstance(Context context) {
+        if (requestQueue == null)
+            requestQueue = Volley.newRequestQueue(context);
+        return requestQueue;
+    }
 }
