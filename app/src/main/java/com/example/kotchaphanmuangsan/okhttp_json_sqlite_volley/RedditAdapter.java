3ec184d504c9a2a48bb757f5bcbb6289aff905e8
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 21:13:48 +0700

    Add row_post layout

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/RedditAdapter.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/RedditAdapter.java
@@ -7,14 +7,15 @@ import android.view.ViewGroup;
  * Created by Kotchaphan Muangsan on 16/8/2558.
  */
 public class RedditAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
     }
 
     @Override
