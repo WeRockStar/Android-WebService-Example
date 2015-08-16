4c8e292ee084805ef5e35ca4db107ab7a0a6645c
comfreestyle <vr_freestyle@outlook.com>
2015-08-17 00:53:54 +0700

    Add Image Loader

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ConnectionManager.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ConnectionManager.java
@@ -1,8 +1,11 @@
 package com.example.kotchaphanmuangsan.okhttp_json_sqlite_volley;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
@@ -11,9 +14,31 @@ import com.android.volley.toolbox.Volley;
 public class ConnectionManager {
     public static RequestQueue requestQueue;
 
+    private static ImageLoader sImageLoader;
+
     public static RequestQueue getInstance(Context context) {
         if (requestQueue == null)
             requestQueue = Volley.newRequestQueue(context);
         return requestQueue;
+    }
+
+    public static ImageLoader getsImageLoader(Context context) {
+        if (sImageLoader == null) {
+            sImageLoader = new ImageLoader(getInstance(context), new ImageLoader.ImageCache() {
+
+                private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return null;
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+
+                }
+            });
+        }
+        return sImageLoader;
     }
 }
