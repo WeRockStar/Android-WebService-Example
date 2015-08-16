9d249faec4b6e490f337ffbd405463f751eb3851
comfreestyle <vr_freestyle@outlook.com>
2015-08-16 23:12:08 +0700

    Write DROP TABLE SQL statements

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/database/DBOpenHelper.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/database/DBOpenHelper.java
@@ -9,9 +9,13 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBOpenHelper extends SQLiteOpenHelper {
 
+    //VERSION
+    private static final int DATABASE_VERSION = 1;
+
     private static final String TEXT_TYPE = "TEXT";
     private static final String INTEGER_TYPE = "INTEGER";
 
+    //SQL Statements
     private static final String CREATE_POST_TABLE = "CREATE TABLE "
             + DatabaseContract.PostTable.TABLE_NAME + " ( "
             + DatabaseContract.PostTable.TITLE + TEXT_TYPE + " , "
@@ -19,17 +23,22 @@ public class DBOpenHelper extends SQLiteOpenHelper {
             + DatabaseContract.PostTable.IMAGELINK + TEXT_TYPE + " )";
 
 
-    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    //DROP POST TABLE
+    public static final String DROP_POST_TABLE = "DROP TABLE IF EXISTS " + DatabaseContract.PostTable.TABLE_NAME;
+
+    //Constructor
+    public DBOpenHelper(Context context) {
+        super(context, DatabaseContract.DB_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_POST_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(DROP_POST_TABLE);
+        onCreate(db);
     }
 }
