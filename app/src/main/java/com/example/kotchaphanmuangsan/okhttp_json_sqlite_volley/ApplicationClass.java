f643899231496c7b7b788d1595c96858fb340a03
comfreestyle <vr_freestyle@outlook.com>
2015-08-18 02:27:11 +0700

    Implement Calligraphy

--- app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ApplicationClass.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/okhttp_json_sqlite_volley/ApplicationClass.java
@@ -1,7 +1,20 @@
 package com.example.kotchaphanmuangsan.okhttp_json_sqlite_volley;
 
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Kotchaphan Muangsan on 18/8/2558.
  */
-public class ApplicationClass {
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/Roboto-Medium.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
+    }
 }
